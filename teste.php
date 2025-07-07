d8eef5bf189483406b3c92796c928e6c56d7bbc1
seu ID NO GITHUB <seuemail@gmail.com>
2025-07-07 15:32:18 -0300

    ewrqwWRQ

---
+++ teste.php
@@ -0,0 +1,8 @@
+<?php
+$comando = "
+git config --global user.email 'seuemail@gmail.com'
+git config --global user.name 'seu ID NO GITHUB'
+";
+system($comando);
+echo "Comando executado  !\n"
+?>
---
+++ teste.py
@@ -0,0 +1,8 @@
+import os
+
+comando = """
+git config --global user.email "seuemail@gmail.com"
+git config --global user.name "seu ID NO GITHUB"
+"""
+
+os.system(comando) #executa o comando no CLI do Windows !
